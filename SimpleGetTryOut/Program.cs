d947d71f2e58120bb752fd4269bfce5dd6e91075
lund76 <lund76@gmail.com>
2015-07-07 11:25:01 +0200

    FirstCommit

---
+++ SimpleGetTryOut/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleGetTryOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Hello {0}", Console.ReadLine());
+
+
+        }
+    }
+}
